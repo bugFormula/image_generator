40efcfff797f35ef975735c11757d90c886952ff
Simon Parker <simon.parker.web@gmail.com>
2016-11-21 17:38:25 +0000

    Update Image_generator.php

--- class/Image_generator.php
+++ class/Image_generator.php
@@ -25,26 +25,26 @@ class Image_generator
   */
 	public function push_images($pImg_dir) {
 		$this->img_dir = $pImg_dir;							
-		$img_array = scandir ($pImg_dir);							//	Scan Folder Contents.
+		$img_array = scandir ($pImg_dir);				//	Scan Folder Contents.
 		$img_array = array_diff($img_array, array('.', '..'));		//	Remove Directory Roots (.,..).
-		$img_array = array_values($img_array);						//	Re-assign Array with new index.
-		foreach ($img_array as $key => $value) {					//	Concatenate image path.
-			$img_array[$key] = $this->img_dir.$value;				//	
+		$img_array = array_values($img_array);				//	Re-assign Array with new index.
+		foreach ($img_array as $key => $value) {			//	Concatenate image path.
+			$img_array[$key] = $this->img_dir.$value;		//	
 		}															//	
-		$this->img_array = $img_array;								//	Re-assign to Class attribute for global scope access.
+		$this->img_array = $img_array;					//	Re-assign to Class attribute for global scope access.
 		return $img_array;											//	Return the Array.		
 	}
 	/**
 	 * @return String
 	 */
 	public function get_img_dir () {
-		return $this->img_dir;										//	(Accessor/Getter) Return Image Folder Name.
+		return $this->img_dir;						//	(Accessor/Getter) Return Image Folder Name.
 	}
 	/**
 	 * @return Array
 	 */
 	public function get_img_array () {
-		return $this->img_array;									// (Accessor/Getter) Return Array Contents.
+		return $this->img_array;					// (Accessor/Getter) Return Array Contents.
 	}
 }
 ?>
