6ddbb199f483941653dc827bc42efdec77504db7
Simon Parker <simonfoxparker@protonmail.com>
2019-10-23 22:44:33 +0100

    Comment tidy up
    Comment tidy up

--- class/Image_generator.php
+++ class/Image_generator.php
@@ -4,9 +4,9 @@
 * @author Simon Parker 22/07/2015
 * @version 0.1
 * @category File Access/Read
-* 
+*
 */
-class Image_generator											
+class Image_generator
 {
 /**
  * [$img_array folder contents]
@@ -24,27 +24,27 @@ class Image_generator
   * @return Array $img_array File Contents
   */
 	public function push_images($pImg_dir) {
-		$this->img_dir = $pImg_dir;							
-		$img_array = scandir ($pImg_dir);				//	Scan Folder Contents.
+		$this->img_dir = $pImg_dir;
+		$img_array = scandir ($pImg_dir);													//	Scan Folder Contents.
 		$img_array = array_diff($img_array, array('.', '..'));		//	Remove Directory Roots (.,..).
-		$img_array = array_values($img_array);				//	Re-assign Array with new index.
-		foreach ($img_array as $key => $value) {			//	Concatenate image path.
-			$img_array[$key] = $this->img_dir.$value;		//	
-		}															//	
-		$this->img_array = $img_array;					//	Re-assign to Class attribute for global scope access.
-		return $img_array;											//	Return the Array.		
+		$img_array = array_values($img_array);										//	Re-assign Array with new index.
+		foreach ($img_array as $key => $value) {									//	Concatenate image path.
+			$img_array[$key] = $this->img_dir.$value;
+		}
+		$this->img_array = $img_array;														//	Re-assign to Class attribute for global scope access.
+		return $img_array;																				//	Return the Array.
 	}
 	/**
 	 * @return String
 	 */
 	public function get_img_dir () {
-		return $this->img_dir;						//	(Accessor/Getter) Return Image Folder Name.
+		return $this->img_dir;																		//	(Accessor/Getter) Return Image Folder Name.
 	}
 	/**
 	 * @return Array
 	 */
 	public function get_img_array () {
-		return $this->img_array;					// (Accessor/Getter) Return Array Contents.
+		return $this->img_array;																	// (Accessor/Getter) Return Array Contents.
 	}
 }
 ?>
